33f7f7cbb001e376048d60f577d63c77f2349388
Donovan Watteau <contrib@dwatteau.fr>
2025-04-18 22:07:37 +0200

    feat(man): In man2txt.sh, warn when I forget to update $Mdocdate

--- man/man2txt.sh
+++ man/man2txt.sh
@@ -3,6 +3,8 @@
 # Convert man pages to simple .txt files, for systems where man pages
 # are not convenient (e.g. Windows).
 #
+# (Also tries to warn of '$Mdocdate' fields needing an update.)
+#
 # Requires: https://mandoc.bsd.lv/
 #
 set -eu
@@ -16,3 +18,17 @@ mkdir -p txt
 for manpage in *.1 ; do
 	mandoc -Tascii -I os=ScummTR "$manpage" | sed "s/\$/$(printf '\r')/; s/.$(printf '\b')//g;" > "txt/${manpage%.1}.txt"
 done
+
+if command -v git >/dev/null 2>&1 ; then
+	modified_files=$(git status -s -uno -- ./txt/ | awk '/\.txt$/ { print $2 }')
+	if [ ! -z "$modified_files" ]; then
+		current_date1=$(LC_ALL=C LC_TIME=C date '+%B %d, %Y')
+		current_date2=$(LC_ALL=C LC_TIME=C date '+%B %e, %Y')
+		for file in $modified_files ; do
+			if ! grep -qF "$current_date1" -- "$file" &&
+			   ! grep -qF "$current_date2" -- "$file" ; then
+				echo "WARNING: Possible missing \$Mdocdate update for: $file" >&2
+			fi
+		done
+	fi
+fi
