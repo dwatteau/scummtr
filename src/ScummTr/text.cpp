0f9f58e40dc6974a5e1a3b220c1ea5f3042cbfb2
Donovan Watteau <contrib@dwatteau.fr>
2025-04-18 22:15:47 +0200

    feat(scummtr): Add my Copyright to text.cpp
    Since latest addition is mine, and non-trivial.

--- src/ScummTr/text.cpp
+++ src/ScummTr/text.cpp
@@ -2,6 +2,7 @@
  * SPDX-License-Identifier: MIT
  *
  * Copyright (c) 2003-2005 Thomas Combeleran
+ * Copyright (c) 2025 Donovan Watteau
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
