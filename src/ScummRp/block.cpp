dwatteau/scummtr

commit dc94b2a5bafdeba50f4e59dd91278610899edd8a

fix(scummrp): Fix fatal "Duplicate offset in index" error for MONKEY1-FLOPPY-VGA
It appears that this version of Monkey Island has CO_0117 as a duplicate
costume for CO_0010.  This would break ScummRp and ScummTr extraction.

As far as I know, the EGA and the CD-ROM versions of Monkey Island 1
don't have this bug.

Issue #18.

src/ScummRp/block.cpp
@@ -782,6 +782,18 @@ void RoomPack::_checkDupOffset(byte roomId, int32 offset)
 				ScummRpIO::info(INF_DETAIL, "Removed SC_0051 from index (duplicate of SC_0052)");
 			}
 		}
+		else if (j == 2 && ScummRp::tocs[i]->getType() == TableOfContent::TOCT_COST)
+		{
+			// Hack for Monkey1 Floppy VGA
+			if (roomId == 59 && ScummRp::tocs[i]->getSize() == 199
+				&& (*ScummRp::tocs[i])[10].offset == (*ScummRp::tocs[i])[117].offset
+				&& (*ScummRp::tocs[i])[10].roomId == (*ScummRp::tocs[i])[117].roomId)
+			{
+				(*ScummRp::tocs[i])[117].offset = -1;
+				j = 1;
+				ScummRpIO::info(INF_DETAIL, "Removed CO_0117 from index (duplicate of CO_0010)");
+			}
+		}
 		n += j;
 		if (n > 1)
 			throw RoomPack::BadOffset(xsprintf("Duplicate offset in index: 0x%X in room %i", offset, roomId));

commit cf88d41b5db5a3289f80a210ae49a64b449b1e37

fix: Unbreak MONKEY1-FLOPPY-VGA releases having duplicate costumes in room 59
Mistake introduced in commit dc94b2a5bafdeba50f4e59dd91278610899edd8a,
but hidden by commit 4f92fe2520ca5ff52c6bd0a8755b42c4b1ef0565 which
put this behind an ifdef which wasn't set by default.

The costume is used at Stan's (room 59), at the end of the game, when
Guybrush falls into the grog machine.  Script 130 uses costume 117,
not costume 10, so the wrong costume was removed, causing the game to
crash when hitting this part of the game.

Issue #31.

src/ScummRp/block.cpp
@@ -816,9 +816,9 @@ void RoomPack::_checkDupOffset(byte roomId, int32 offset)
 				// XXX: may actually be fine, and "just" be a ScummVM detection fallback issue
 				// (such as: VGA title misdetected as EGA, in absence of a detection table entry
 				// for it).
-				(*ScummRp::tocs[i])[117].offset = -1;
+				(*ScummRp::tocs[i])[10].offset = -1;
 				j = 1;
-				ScummIO::info(INF_DETAIL, "Removed CO_0117 from index (duplicate of CO_0010)");
+				ScummIO::info(INF_DETAIL, "Removed CO_0010 from index (duplicate of CO_0117)");
 #else
 				ScummIO::fatal("This version of Monkey Island 1 is currently unsupported because of a corruption bug");
 #endif